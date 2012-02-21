20a848cd5cb88a52afe6c29c54f2b54bef5947e9
Jaroslav Keznikl <jaroslav.keznikl@gmail.com>
2012-02-21 11:29:43 +0100

    refactoring

---
+++ src/Framework.groovy
@@ -0,0 +1,200 @@
+import groovy.lang.Closure;
+
+import java.util.List;
+import java.util.Map;
+
+
+
+import groovyx.gpars.actor.Actor
+import groovyx.gpars.actor.BlockingActor;
+import groovyx.gpars.actor.DefaultActor
+
+class KnowledgeListener {
+	Actor actor
+	List fields
+}
+
+class KnowledgeActor extends DefaultActor {
+	def name
+	def knowledge
+	def List listeners = []
+	
+	void registerListener(KnowledgeListener l) {
+		listeners.add(l)
+	}
+	
+	private assembleChangeSet(List fields) {
+		def msg = [:]
+		if (fields == ["root"])
+			msg = ["root": knowledge]
+		else {
+			for (f in fields) {
+				msg[f] = knowledge[f]
+			}
+		}
+		return msg
+	}
+	private void notifyListener(KnowledgeListener l) {
+		l.actor.send assembleChangeSet(l.fields)
+	}
+	
+	private void processDataRequest(Actor a, List fields) {
+		a.send(assembleChangeSet(fields))
+	}
+	
+	void processChangeSet(Map changeSet) {
+		def changed = []
+		for (key in changeSet.keySet()) {
+			if (knowledge[key] != changeSet[key]) {
+				knowledge[key] = changeSet[key]
+				changed.add(key)
+			}
+		}
+		 
+		if (changed != []) {
+			def toNotify = listeners.findAll { KnowledgeListener l ->
+				l.fields == ["root"] || !l.fields.disjoint(changed)
+			}
+			for (l in toNotify) {
+				notifyListener(l)
+			}
+		}
+	}
+
+	void act() {
+		loop {
+			react { msg ->
+				if (msg instanceof Map)
+					processChangeSet(msg as Map)
+				else if (msg instanceof ReqDataMessage)
+					processDataRequest(sender, ((ReqDataMessage)msg).fields)
+				else
+					System.err.println("Unknown message: " + msg.toString());
+				
+			}
+		}
+	}
+}
+
+
+class TriggeredProcessActor extends DefaultActor {
+	def func
+	def List inMapping
+	def List outMapping
+
+	void act() {
+		loop {
+			react { Map args ->
+				if (args.keySet().equals(inMapping.toSet())) {
+					def argList = []
+					for (key in inMapping)
+						argList.add(args[key])
+					
+					if (argList.size() == 1 && argList.first() instanceof List)
+						argList = argList.first()
+							
+					def resultList = func(argList)
+						
+					if (resultList != null && resultList != [] && outMapping != []) {
+						def result = [outMapping, resultList].transpose().collectEntries { it }
+						System.out.println("result: " + result)
+						reply result
+					}
+				}
+			}
+		}
+	}
+}
+
+class ReqDataMessage {
+	def List fields
+}
+
+class PeriodicProcessActor extends BlockingActor{
+	def long sleepTime
+	def func
+	def KnowledgeActor knowledgeActor
+	def List inMapping
+	def List outMapping
+
+	void act() {
+		while (true) {
+			knowledgeActor.send new ReqDataMessage(fields: inMapping)
+			def args = receive()
+			def argList = []
+			for (key in inMapping)
+				argList.add(args[key])
+			def resultList = func(argList)
+			
+			if (resultList != null && resultList != [] && outMapping != []) {
+				def result = [outMapping, resultList].transpose().collectEntries { it }
+				knowledgeActor.send result
+			}
+			sleep(sleepTime)
+		}
+	}
+}
+
+
+
+enum SchedType {PROCESS_PERIODIC, PROCESS_TRIGGERED}
+class IProcess {
+	SchedType schedType
+	Closure func
+	List inMapping = []
+	List outMapping = []
+	Map schedData = [:]
+}
+
+
+
+class Framework { 
+	def framework = new TriggeredProcessActor(
+		func: this.&FrameworkF,
+		inMapping: ["root"],
+		outMapping: []
+	).start()
+	
+	def runComponent(Map r) {
+		def startedActors = []
+		
+		def KnowledgeActor k = new KnowledgeActor(
+			name: r.id,
+			knowledge: r
+		).start()
+		
+		k.registerListener([actor: framework, fields: framework.inMapping] as KnowledgeListener)
+		
+		startedActors.add(k)
+		
+		for (p in r.processes) {
+			IProcess pr = p.value
+			if (pr.schedType == SchedType.PROCESS_TRIGGERED) {
+				def pa = new TriggeredProcessActor(
+					func: pr.func,
+					inMapping: pr.inMapping,
+					outMapping: pr.outMapping
+				).start()
+				k.registerListener([actor: pa, fields: pr.inMapping] as KnowledgeListener)
+				startedActors.add(pa)
+									
+			} else if (pr.schedType == SchedType.PROCESS_PERIODIC) {
+				def pa = new PeriodicProcessActor(
+					func: pr.func,
+					inMapping: pr.inMapping,
+					outMapping: pr.outMapping,
+					knowledgeActor: k,
+					sleepTime: pr.schedData.sleepTime
+				).start()
+				startedActors.add(pa)
+			} else {
+				System.err.println("Unknown process type ${pr.schedType} for process ${p.key}");
+			}
+		}
+		return startedActors
+	}
+	
+	private FrameworkF(component) {
+		System.out.println("Framework update of ${component.id}")
+	}
+}
--- src/deecotest.groovy
+++ src/deecotest.groovy
@@ -1,87 +1,63 @@
 import actors.*
+import Framework.*
 import groovyx.gpars.actor.Actor
 
 
 class IPosition {
     def x
     def y
+	String toString() { return "[$x, $y]" }
+	int hashCode() { return 1000*x + y}
+	boolean equals(IPosition p) { this.hashCode() == p.hashCode() } 
 }
 
-enum SchedType {PROCESS_PERIODIC, PROCESS_TRIGGERED}
-class IProcess {
-	SchedType schedType
-	Closure func
-	List inMapping = []
-	List outMapping = []
-	Map schedData = [:]	
-}
 
-def RobotStepF(position, List path) {
+
+def RobotStepF(nextPosition, path) {
+	if (path.size()>0 && path.first().equals(nextPosition))	
+		return [nextPosition, path.drop(1)]	
+	else
+		return [nextPosition, path]
+}
+def RobotDriveF(List path) {
 	if (path.size() > 0)
-		return [path.first(), path.drop(1)]	
+		return [path.first()]
 }
-
 def LogPosition(id, position) { System.out.println("Position for $id updated to: [${position.x}, ${position.y}]");}
 
 robot = [
     id: "R1",
     position: [x: 1, y: 1] as IPosition,
-    path: [[x:1, y:2] as IPosition, [x:1, y:3] as IPosition, [x:2, y:3] as IPosition], 
-	processes: [
-		step: new IProcess(
-			schedType: SchedType.PROCESS_PERIODIC,
-			func: this.&RobotStepF, 
-			inMapping: ["position", "path"], 
-			outMapping: ["position", "path"],			
-			schedData: [sleepTime: 1000]),
+	nextPosition: [x: 1, y: 2] as IPosition,
+    path: [[x: 1, y: 2] as IPosition, [x:1, y:3] as IPosition, [x:1, y:4] as IPosition, [x:2, y:4] as IPosition], 
+	processes: [		
+		drive: new IProcess(
+			schedType: SchedType.PROCESS_TRIGGERED,
+			func: this.&RobotDriveF,
+			inMapping: ["path"], 
+			outMapping: ["nextPosition"]),				
 		log: new IProcess(
 			schedType: SchedType.PROCESS_TRIGGERED,
 			func: this.&LogPosition,
 			inMapping: ["id", "position"], 
-			outMapping: [])
+			outMapping: []),
+		step: new IProcess(
+			schedType: SchedType.PROCESS_PERIODIC,
+			func: this.&RobotStepF,
+			inMapping: ["nextPosition", "path"],
+			outMapping: ["position", "path"],
+			schedData: [sleepTime: 1000]),
 		
 	]
 ]
 
 
-def runComponent(Map r) {
-	def startedActors = []	
-	
-	def KnowledgeActor k = new KnowledgeActor(
-		name: r.id,
-		knowledge: r
-	).start()
-	
-	startedActors.add(k)
-	
-	for (p in r.processes) {
-		IProcess pr = p.value
-		if (pr.schedType == SchedType.PROCESS_TRIGGERED) {
-			def pa = new TriggeredProcessActor(
-				func: pr.func,
-				inMapping: pr.inMapping,
-				outMapping: pr.outMapping
-			).start()
-			k.registerListener([actor: pa, fields: pr.inMapping] as KnowledgeListener)	
-			startedActors.add(pa)
-								
-		} else if (pr.schedType == SchedType.PROCESS_PERIODIC) {
-			def pa = new PeriodicProcessActor(
-				func: pr.func,
-				inMapping: pr.inMapping,
-				outMapping: pr.outMapping,
-				knowledgeActor: k,
-				sleepTime: pr.schedData.sleepTime
-			).start()
-			startedActors.add(pa)
-		} else {
-			System.err.println("Unknown process type ${pr.schedType} for process ${p.key}");
-		}		
-	}
-	return startedActors
-}
 
-runComponent(robot)*.join()
+
+
+
+def f = new Framework()
+f.runComponent(robot)*.join()
 
 //def k = new KnowledgeActor(
 //	name:"R1", 
